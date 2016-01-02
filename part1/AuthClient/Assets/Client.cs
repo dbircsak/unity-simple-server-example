4de0a9474776b7e4fd9c96d386e05194d6adc825
Darrell Bircsak <dbircsak@gmail.com>
2016-01-01 21:59:36 -0800

    First try at this

---
+++ part1/AuthClient/Assets/Client.cs
@@ -0,0 +1,155 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Client : MonoBehaviour
+{
+    int m_hostId = -1;
+    bool connected = false;
+
+    void Start()
+    {
+        Application.runInBackground = true;
+        NetworkTransport.Init();
+
+        ConnectionConfig config = new ConnectionConfig();
+        config.AddChannel(QosType.Reliable);
+        HostTopology topology = new HostTopology(config, 1); // Just need 1 connection
+#if UNITY_EDITOR
+        m_hostId = NetworkTransport.AddHostWithSimulator(topology, 200, 400);
+#else
+        m_hostId = NetworkTransport.AddHost(topology);
+#endif
+
+        byte error;
+        // Cannot tell we're connected until we receive the event at later time
+        NetworkTransport.Connect(m_hostId, System.Net.IPAddress.Loopback.ToString(), 25000, 0, out error);
+    }
+
+    struct SphereData
+    {
+        public float lastUpdate;
+        public GameObject obj;
+    }
+    // Remeber all spheres we spawned
+    Dictionary<int, SphereData> sphereDictionary = new Dictionary<int, SphereData>();
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), string.Format("{0} Spheres", sphereDictionary.Count));
+        if (connected) // Set when ConnectEvent received
+            GUI.Label(new Rect(10, 30, 100, 20), "Connected");
+        else
+            GUI.Label(new Rect(10, 30, 100, 20), "Disconnected");
+    }
+
+    struct NetworkData
+    {
+        public int id;
+        public float x;
+        public float y;
+        public float z;
+        public float r;
+        public float g;
+        public float b;
+    }
+
+    void Update()
+    {
+        if (m_hostId == -1)
+            return;
+        int connectionId;
+        int channelId;
+        int receivedSize;
+        byte error;
+        byte[] buffer = new byte[1500];
+        NetworkEventType networkEvent = NetworkTransport.ReceiveFromHost(m_hostId, out connectionId, out channelId, buffer, buffer.Length, out receivedSize, out error);
+        switch (networkEvent)
+        {
+            case NetworkEventType.Nothing:
+                break;
+            case NetworkEventType.ConnectEvent:
+                connected = true;
+                break;
+            case NetworkEventType.DisconnectEvent:
+                connected = false;
+                break;
+            case NetworkEventType.DataEvent:
+                //Debug.Log(string.Format("Got data size {0}", receivedSize));
+                // Trim buffer from original size 1500
+                Array.Resize(ref buffer, receivedSize);
+                LoadSpheres(buffer);
+                break;
+        }
+
+        CleanupSpheres();
+    }
+
+    void LoadSpheres(byte[] buffer)
+    {
+        MemoryStream stream = new MemoryStream(buffer);
+        // Don't use BinaryFormatter as this stores meta data!!
+        BinaryReader br = new BinaryReader(stream);
+
+        List<NetworkData> tmpList = new List<NetworkData>();
+        NetworkData tmpData;
+
+        // Read to the end
+        while (br.PeekChar() != -1)
+        {
+            // Deserialize
+            tmpData.id = br.ReadInt32();
+            tmpData.x = br.ReadSingle();
+            tmpData.y = br.ReadSingle();
+            tmpData.z = br.ReadSingle();
+            tmpData.r = br.ReadSingle();
+            tmpData.g = br.ReadSingle();
+            tmpData.b = br.ReadSingle();
+            tmpList.Add(tmpData);
+        }
+
+        SphereData sd;
+        foreach (var networkData in tmpList)
+        {
+            // Do we have this sphere already?
+            if (sphereDictionary.ContainsKey(networkData.id))
+            {
+                sd = sphereDictionary[networkData.id];
+                // Update position
+                sd.obj.transform.position = new Vector3(networkData.x, networkData.y, networkData.z);
+                sd.lastUpdate = Time.realtimeSinceStartup;
+                sphereDictionary[networkData.id] = sd;
+            }
+            else
+            {
+                // Create new object
+                GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                // No rigidbody here!
+                // Set position
+                sphere.transform.position = new Vector3(networkData.x, networkData.y, networkData.z);
+                // Set color
+                sphere.GetComponent<Renderer>().material.color = new Color(networkData.r, networkData.g, networkData.b);
+
+                sd.obj = sphere;
+                sd.lastUpdate = Time.realtimeSinceStartup;
+                sphereDictionary.Add(networkData.id, sd);
+            }
+        }
+    }
+
+    void CleanupSpheres()
+    {
+        // Clean up null game objects
+        sphereDictionary = sphereDictionary.Where(pair => pair.Value.obj.gameObject != null).ToDictionary(pair => pair.Key, pair => pair.Value); // Ahhh Ling!
+
+        foreach (var pair in sphereDictionary)
+        {
+            // Haven't heard about sphere in a while so destroy it
+            if (pair.Value.obj.gameObject != null && pair.Value.lastUpdate + 1.0f < Time.realtimeSinceStartup)
+                Destroy(pair.Value.obj.gameObject); // Note this becomes null only after Update
+        }
+    }
+}
---
+++ part1/AuthServer/Assets/Server.cs
@@ -0,0 +1,163 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Types;
+using System.IO;
+using System.Net;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Server : MonoBehaviour
+{
+    byte reliableChannel;
+    int m_hostId = -1;
+    Dictionary<int, IPEndPoint> connectDictionary = new Dictionary<int, IPEndPoint>();
+
+    // Remeber all spheres we spawned
+    List<GameObject> sphereList = new List<GameObject>();
+
+    void Start()
+    {
+        Application.runInBackground = true;
+        NetworkTransport.Init();
+
+        ConnectionConfig config = new ConnectionConfig();
+        reliableChannel = config.AddChannel(QosType.Reliable);
+        HostTopology topology = new HostTopology(config, 5);
+#if UNITY_EDITOR
+        m_hostId = NetworkTransport.AddHostWithSimulator(topology, 200, 400, 25000);
+#else
+        m_hostId = NetworkTransport.AddHost(topology, 25000);
+#endif
+
+        Physics.gravity = new Vector3(); // No gravity
+
+        // Spawn a sphere every so often
+        StartCoroutine(SpawnRandomCoroutine());
+        // Send sphere data every so often to those connected
+        StartCoroutine(SendCoroutine());
+    }
+
+    // Send sphere data every so often to those connected
+    IEnumerator SendCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f);
+
+            // Anything to do?
+            if (connectDictionary.Count == 0 || sphereList.Count == 0)
+                continue;
+
+            // Serialize list
+            MemoryStream stream = new MemoryStream();
+            // Don't use BinaryFormatter as this stores meta data!!
+            BinaryWriter bw = new BinaryWriter(stream);
+            Renderer rend;
+            foreach (var item in sphereList)
+            {
+                bw.Write(item.GetInstanceID());
+                bw.Write(item.transform.position.x);
+                bw.Write(item.transform.position.y);
+                bw.Write(item.transform.position.z);
+                rend = item.GetComponent<Renderer>();
+                bw.Write(rend.material.color.r);
+                bw.Write(rend.material.color.g);
+                bw.Write(rend.material.color.b);
+
+                // Don't pack too much
+                // Fix me! Send more than one packet instead
+                if (stream.Position > 1300)
+                    break;
+            }
+
+            // Send data out
+            byte[] buffer = stream.ToArray();
+            byte error;
+            //Debug.Log(string.Format("Sending data size {0}", buffer.Length));
+            foreach (var pair in connectDictionary)
+            {
+                NetworkTransport.Send(m_hostId, pair.Key, reliableChannel, buffer, buffer.Length, out error);
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), string.Format("{0} Spheres", sphereList.Count));
+    }
+
+    void Update()
+    {
+        // Let user spawn a sphere if they want
+        if (Input.GetKey(KeyCode.Space))
+        {
+            StartCoroutine(SpawnCoroutine());
+        }
+
+        // Remember who's connecting and disconnecting to us
+        if (m_hostId == -1)
+            return;
+        int connectionId;
+        int channelId;
+        int receivedSize;
+        byte error;
+        byte[] buffer = new byte[1500];
+        NetworkEventType networkEvent = NetworkTransport.ReceiveFromHost(m_hostId, out connectionId, out channelId, buffer, buffer.Length, out receivedSize, out error);
+        switch (networkEvent)
+        {
+            case NetworkEventType.Nothing:
+                break;
+            case NetworkEventType.ConnectEvent:
+                string address;
+                int port;
+                NetworkID network;
+                NodeID dstNode;
+                NetworkTransport.GetConnectionInfo(m_hostId, connectionId, out address, out port, out network, out dstNode, out error);
+                connectDictionary.Add(connectionId, new IPEndPoint(IPAddress.Parse(address), port));
+                Debug.Log(string.Format("Client connected", connectionId));
+                break;
+            case NetworkEventType.DisconnectEvent:
+                connectDictionary.Remove(connectionId);
+                Debug.Log(string.Format("Client disconnected", connectionId));
+                break;
+            case NetworkEventType.DataEvent:
+                Debug.Log(string.Format("Got data size {0}", receivedSize));
+                break;
+        }
+    }
+
+    IEnumerator SpawnCoroutine()
+    {
+        if (sphereList.Count > 50)
+        {
+            Debug.Log(string.Format("Spawning too many spheres to send over the network"));
+            yield break;
+        }
+
+        // Create new object
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.AddComponent<Rigidbody>();
+        //sphere.GetComponent<Rigidbody>().velocity = new Vector3(UnityEngine.Random.Range(-1.0f, 1.0f), UnityEngine.Random.Range(-1.0f, 1.0f), UnityEngine.Random.Range(-1.0f, 1.0f));
+        // Set position
+        sphere.transform.position = new Vector3(UnityEngine.Random.Range(-2.0f, 2.0f), UnityEngine.Random.Range(-2.0f, 2.0f), UnityEngine.Random.Range(-2.0f, 2.0f));
+        // Set color
+        sphere.GetComponent<Renderer>().material.color = new Color(UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value);
+
+        // Save data for when we send across the net
+        sphereList.Add(sphere);
+        yield return new WaitForSeconds(5.0f);
+
+        sphereList.Remove(sphere);
+        Destroy(sphere);
+    }
+
+    // Spawn a sphere every so often
+    IEnumerator SpawnRandomCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(UnityEngine.Random.value);
+            StartCoroutine(SpawnCoroutine());
+        }
+    }
+}
