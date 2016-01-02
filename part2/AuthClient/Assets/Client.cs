539092c316bbacb7a046ddeb6735c11af7e0a046
Darrell Bircsak <dbircsak@gmail.com>
2016-01-02 12:58:42 -0800

    Part 2 added

---
+++ part2/AuthClient/Assets/Client.cs
@@ -0,0 +1,262 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Types;
+using System;
+using System.IO;
+using System.Net;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Client : MonoBehaviour
+{
+    byte reliableChannel;
+    int m_hostId = -1;
+    int m_serverConnectionId = -1;
+
+    class ClientData
+    {
+        public int objectId;
+        public string name;
+        public GameObject obj;
+    }
+    List<ClientData> clientList = new List<ClientData>();
+
+    void Start()
+    {
+        Application.runInBackground = true;
+        NetworkTransport.Init();
+
+        ConnectionConfig config = new ConnectionConfig();
+        reliableChannel = config.AddChannel(QosType.Reliable);
+        HostTopology topology = new HostTopology(config, 1);
+#if UNITY_EDITOR
+        m_hostId = NetworkTransport.AddHostWithSimulator(topology, 200, 400);
+#else
+        m_hostId = NetworkTransport.AddHost(topology);
+#endif
+
+        byte error;
+        // Cannot tell we're connected until we receive the event at later time
+        NetworkTransport.Connect(m_hostId, IPAddress.Loopback.ToString(), 25000, 0, out error);
+
+        // Send input data every so often
+        StartCoroutine(SendInputCoroutine());
+    }
+
+    void OnGUI()
+    {
+        if (m_serverConnectionId != -1) // Set when ConnectEvent received
+            GUI.Label(new Rect(10, 10, 100, 20), "Connected");
+        else
+            GUI.Label(new Rect(10, 10, 100, 20), "Disconnected");
+    }
+
+    void Update()
+    {
+        if (m_hostId == -1)
+            return;
+        int connectionId;
+        int channelId;
+        int receivedSize;
+        byte error;
+        byte[] buffer = new byte[1500];
+        NetworkEventType networkEvent = NetworkTransport.ReceiveFromHost(m_hostId, out connectionId, out channelId, buffer, buffer.Length, out receivedSize, out error);
+        switch (networkEvent)
+        {
+            case NetworkEventType.Nothing:
+                break;
+            case NetworkEventType.ConnectEvent:
+                m_serverConnectionId = connectionId;
+                break;
+            case NetworkEventType.DisconnectEvent:
+                m_serverConnectionId = -1;
+                break;
+            case NetworkEventType.DataEvent:
+                if (connectionId != m_serverConnectionId)
+                {
+                    Debug.Log("Data not from server!?");
+                }
+                else
+                {
+                    Array.Resize(ref buffer, receivedSize);
+                    ProcessServerData(buffer);
+                }
+                break;
+        }
+    }
+
+    enum PacketTypeEnum
+    {
+        Unknown = (1 << 0),
+        Position = (1 << 1),
+        Information = (1 << 2)
+    }
+
+    class PositionData
+    {
+        public int objectId;
+        public float x;
+        public float y;
+        public float z;
+    }
+
+    class InformationData
+    {
+        public int objectId;
+        public string name;
+        public float x;
+        public float y;
+        public float z;
+        public float r;
+        public float g;
+        public float b;
+    }
+
+    void ProcessServerData(byte[] buffer)
+    {
+        MemoryStream stream = new MemoryStream(buffer);
+        BinaryReader br = new BinaryReader(stream);
+
+        PacketTypeEnum packetType = (PacketTypeEnum)br.ReadByte();
+        switch (packetType)
+        {
+            case PacketTypeEnum.Position:
+                List<PositionData> posList = new List<PositionData>();
+                PositionData p;
+                while (stream.Position != buffer.Length)
+                {
+                    p = new PositionData();
+                    p.objectId = br.ReadInt32();
+                    p.x = br.ReadSingle();
+                    p.y = br.ReadSingle();
+                    p.z = br.ReadSingle();
+                    posList.Add(p);
+                }
+
+                // Update game objects
+                foreach (var item in clientList)
+                {
+                    if (item.obj == null)
+                        continue;
+                    p = posList.FirstOrDefault(x => x.objectId == item.objectId);
+                    if (p == null)
+                        Debug.Log("Cannot find game object");
+                    else
+                        item.obj.transform.position = new Vector3(p.x, p.y, p.z);
+                }
+                break;
+            case PacketTypeEnum.Information:
+                List<InformationData> infoList = new List<InformationData>();
+                InformationData info;
+                while (stream.Position != buffer.Length)
+                {
+                    info = new InformationData();
+                    info.objectId = br.ReadInt32();
+                    info.name = br.ReadString();
+                    info.x = br.ReadSingle();
+                    info.y = br.ReadSingle();
+                    info.z = br.ReadSingle();
+                    info.r = br.ReadSingle();
+                    info.g = br.ReadSingle();
+                    info.b = br.ReadSingle();
+                    infoList.Add(info);
+                }
+
+                // Remove clients that aren't listed
+                foreach (var item in clientList)
+                {
+                    if (item.obj == null)
+                        continue;
+                    info = infoList.FirstOrDefault(x => x.objectId == item.objectId);
+                    if (info == null)
+                        Destroy(item.obj);
+                }
+                clientList.RemoveAll(x => x.obj == null); // Note items are set to null only after Update!
+
+                foreach (var item in infoList)
+                {
+                    ClientData cd = clientList.FirstOrDefault(x => x.objectId == item.objectId);
+                    // Is this new client info?
+                    if (cd == null)
+                    {
+                        // Create new object
+                        GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                        // No CharacterController here!
+                        // Set position
+                        obj.transform.position = new Vector3(item.x, item.y, item.z);
+                        // Set color
+                        obj.GetComponent<Renderer>().material.color = new Color(item.r, item.g, item.b);
+
+                        cd = new ClientData();
+                        cd.objectId = item.objectId;
+                        cd.name = item.name;
+                        cd.obj = obj;
+                        clientList.Add(cd);
+                        Debug.Log(string.Format("New client info for {0}", cd.name));
+                    }
+                }
+                break;
+            default:
+                Debug.Log("Unknown packet type");
+                break;
+        }
+    }
+
+    enum InputTypeEnum
+    {
+        KeyNone = (1 << 0),
+        KeyUp = (1 << 1),
+        KeyDown = (1 << 2),
+        KeyLeft = (1 << 3),
+        KeyRight = (1 << 4),
+        KeyJump = (1 << 5)
+    }
+
+    // Send input data every so often
+    IEnumerator SendInputCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f);
+
+            // Anything to do?
+            if (m_hostId == -1 || m_serverConnectionId == -1)
+                continue;
+
+            InputTypeEnum input = InputTypeEnum.KeyNone;
+            float f;
+            f = Input.GetAxis("Horizontal");
+            if (f > 0.0f)
+                input |= InputTypeEnum.KeyUp;
+            if (f < 0.0f)
+                input |= InputTypeEnum.KeyDown;
+            f = Input.GetAxis("Vertical");
+            if (f > 0.0f)
+                input |= InputTypeEnum.KeyRight;
+            if (f < 0.0f)
+                input |= InputTypeEnum.KeyLeft;
+            if (Input.GetKey(KeyCode.Space))
+                input |= InputTypeEnum.KeyJump;
+
+            if (input == InputTypeEnum.KeyNone)
+                continue;
+
+            // Send data out
+            byte[] buffer = new byte[1];
+            buffer[0] = (byte)input;
+            byte error;
+            NetworkTransport.Send(m_hostId, m_serverConnectionId, reliableChannel, buffer, buffer.Length, out error);
+        }
+    }
+
+    void OnApplicationQuite()
+    {
+        // Gracefully disconnect
+        if (m_hostId != -1 && m_serverConnectionId != -1)
+        {
+            byte error;
+            NetworkTransport.Disconnect(m_hostId, m_serverConnectionId, out error);
+        }
+    }
+}
---
+++ part2/AuthServer/Assets/Server.cs
@@ -0,0 +1,245 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Types;
+using System;
+using System.IO;
+using System.Net;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Server : MonoBehaviour
+{
+    byte reliableChannel;
+    int m_hostId = -1;
+
+    class ClientData
+    {
+        public int connectionId;
+        public string name;
+        public GameObject obj;
+    }
+    List<ClientData> clientList = new List<ClientData>();
+
+    void Start()
+    {
+        Application.runInBackground = true;
+        NetworkTransport.Init();
+
+        ConnectionConfig config = new ConnectionConfig();
+        reliableChannel = config.AddChannel(QosType.Reliable);
+        HostTopology topology = new HostTopology(config, 5);
+#if UNITY_EDITOR
+        m_hostId = NetworkTransport.AddHostWithSimulator(topology, 200, 400, 25000);
+#else
+        m_hostId = NetworkTransport.AddHost(topology, 25000);
+#endif
+
+        // Send client position data every so often to those connected
+        StartCoroutine(SendPositionCoroutine());
+    }
+
+    void Update()
+    {
+        // Remember who's connecting and disconnecting to us
+        if (m_hostId == -1)
+            return;
+        int connectionId;
+        int channelId;
+        int receivedSize;
+        byte error;
+        byte[] buffer = new byte[1500];
+        NetworkEventType networkEvent = NetworkTransport.ReceiveFromHost(m_hostId, out connectionId, out channelId, buffer, buffer.Length, out receivedSize, out error);
+        switch (networkEvent)
+        {
+            case NetworkEventType.Nothing:
+                break;
+            case NetworkEventType.ConnectEvent:
+                ClientConnected(connectionId);
+                break;
+            case NetworkEventType.DisconnectEvent:
+                ClientData cd = clientList.FirstOrDefault(item => item.connectionId == connectionId);
+                if (cd != null)
+                {
+                    Destroy(cd.obj);
+                    clientList.Remove(cd);
+                    Debug.Log("Client disconnected");
+                    // Send all clients new info
+                    SendClientInformation();
+                }
+                else
+                {
+                    Debug.Log("Client disconnected that we didn't know about!?");
+                }
+                break;
+            case NetworkEventType.DataEvent:
+                //Debug.Log(string.Format("Got data size {0}", receivedSize));
+                Array.Resize(ref buffer, receivedSize);
+                ProcessClientInput(connectionId, buffer);
+                break;
+        }
+    }
+
+    void ClientConnected(int connectionId)
+    {
+        string address;
+        int port;
+        NetworkID network;
+        NodeID dstNode;
+        byte error;
+        NetworkTransport.GetConnectionInfo(m_hostId, connectionId, out address, out port, out network, out dstNode, out error);
+
+        // Create new object
+        GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        obj.AddComponent<CharacterController>();
+        // Set position
+        obj.transform.position = new Vector3(UnityEngine.Random.Range(-5.0f, 5.0f), 0, UnityEngine.Random.Range(-5.0f, 5.0f));
+        // Set color
+        obj.GetComponent<Renderer>().material.color = new Color(UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value);
+
+        ClientData cd = new ClientData();
+        cd.connectionId = connectionId;
+        cd.name = new IPEndPoint(IPAddress.Parse(address), port).ToString();
+        cd.obj = obj;
+        // Remember client
+        clientList.Add(cd);
+        Debug.Log(string.Format("Client {0} connected", cd.name));
+
+        // Send all clients new info
+        SendClientInformation();
+    }
+
+    enum PacketTypeEnum
+    {
+        Unknown = (1 << 0),
+        Position = (1 << 1),
+        Information = (1 << 2)
+    }
+
+    void SendClientInformation()
+    {
+        MemoryStream stream = new MemoryStream();
+        BinaryWriter bw = new BinaryWriter(stream);
+        Renderer rend;
+
+        bw.Write((byte)PacketTypeEnum.Information);
+        foreach (var item in clientList)
+        {
+            bw.Write(item.obj.GetInstanceID());
+            bw.Write(item.name);
+            bw.Write(item.obj.transform.position.x);
+            bw.Write(item.obj.transform.position.y);
+            bw.Write(item.obj.transform.position.z);
+            rend = item.obj.GetComponent<Renderer>();
+            bw.Write(rend.material.color.r);
+            bw.Write(rend.material.color.g);
+            bw.Write(rend.material.color.b);
+
+            // Don't pack too much
+            // Fix me! Send more than one packet instead
+            if (stream.Position > 1300)
+                break;
+        }
+
+        // Send data out
+        byte[] buffer = stream.ToArray();
+        byte error;
+        //Debug.Log(string.Format("Sending data size {0}", buffer.Length));
+        foreach (var item in clientList)
+        {
+            NetworkTransport.Send(m_hostId, item.connectionId, reliableChannel, buffer, buffer.Length, out error);
+        }
+    }
+
+    enum InputTypeEnum
+    {
+        KeyNone = (1 << 0),
+        KeyUp = (1 << 1),
+        KeyDown = (1 << 2),
+        KeyLeft = (1 << 3),
+        KeyRight = (1 << 4),
+        KeyJump = (1 << 5)
+    }
+
+    void ProcessClientInput(int connectionId, byte[] buffer)
+    {
+        ClientData cd = clientList.FirstOrDefault(item => item.connectionId == connectionId);
+        if (cd == null)
+        {
+            Debug.Log("Client input that we didn't know about!?");
+            return;
+        }
+
+        InputTypeEnum input = (InputTypeEnum)buffer[0];
+        float deltaX = 0.0f;
+        float deltaZ = 0.0f;
+        if ((input & InputTypeEnum.KeyUp) == InputTypeEnum.KeyUp)
+            deltaX = 1.0f;
+        if ((input & InputTypeEnum.KeyDown) == InputTypeEnum.KeyDown)
+            deltaX = -1.0f;
+        if ((input & InputTypeEnum.KeyRight) == InputTypeEnum.KeyRight)
+            deltaZ = 1.0f;
+        if ((input & InputTypeEnum.KeyLeft) == InputTypeEnum.KeyLeft)
+            deltaZ = -1.0f;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = transform.TransformDirection(movement);
+        movement *= 10.0f;
+        cd.obj.GetComponent<CharacterController>().Move(movement * Time.deltaTime);
+    }
+
+    // Send client position data every so often to those connected
+    IEnumerator SendPositionCoroutine()
+    {
+        MemoryStream stream = new MemoryStream();
+        BinaryWriter bw = new BinaryWriter(stream);
+
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f);
+
+            // Anything to do?
+            if (clientList.Count == 0)
+                continue;
+
+            // Reset stream
+            stream.SetLength(0);
+
+            bw.Write((byte)PacketTypeEnum.Position);
+            foreach (var item in clientList)
+            {
+                bw.Write(item.obj.GetInstanceID());
+                bw.Write(item.obj.transform.position.x);
+                bw.Write(item.obj.transform.position.y);
+                bw.Write(item.obj.transform.position.z);
+
+                // Don't pack too much
+                // Fix me! Send more than one packet instead
+                if (stream.Position > 1300)
+                    break;
+            }
+
+            // Send data out
+            byte[] buffer = stream.ToArray();
+            byte error;
+            //Debug.Log(string.Format("Sending data size {0}", buffer.Length));
+            foreach (var item in clientList)
+            {
+                NetworkTransport.Send(m_hostId, item.connectionId, reliableChannel, buffer, buffer.Length, out error);
+            }
+        }
+    }
+
+    void OnApplicationQuite()
+    {
+        // Gracefully disconnect
+        if (m_hostId != -1 && clientList.Count > 0)
+        {
+            byte error;
+
+            foreach (var item in clientList)
+            {
+                NetworkTransport.Disconnect(m_hostId, item.connectionId, out error);
+            }
+        }
+    }
+}
